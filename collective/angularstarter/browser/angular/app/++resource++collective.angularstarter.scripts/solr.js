f285adbe280d71f93c622e1dba89d8b50fc36d23
Rodrigo Ristow <rodrigo.ristow@fhnw.ch>
2015-08-20 16:47:35 +0200

    AutoSearch on Page Load

--- collective/angularstarter/browser/angular/app/++resource++collective.angularstarter.scripts/solr.js
+++ collective/angularstarter/browser/angular/app/++resource++collective.angularstarter.scripts/solr.js
@@ -96,6 +96,10 @@ var solr = angular.module("solr", [])
       if (scope.preload){
         scope.search(scope.query, scope.rows);
       }
+      scope.autoClicked = function () {
+        scope.search(scope.query, scope.rows);
+      }
+
     }
 
   }
@@ -271,4 +275,18 @@ var solr = angular.module("solr", [])
       ctrl.solrUrl = scope.solrUrl;
     }
   };
-});
+})
+
+.directive("onLoadClicker", function($timeout) {
+  return {
+    restrict: "A",
+    priority: -1,
+    link: function(scope, iElm, iAttrs, controller) {
+        $timeout(function() {
+          iElm.triggerHandler('click');
+        }, 0);
+      }
+    };
+  }
+);
+
